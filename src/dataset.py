192bcf1a0f4a7760570dbdaf6d2a30b791879c21
naamashiponi <naamashiponi11@gmail.com>
2024-08-06 13:51:07 +0300

    Data arranged for SAVEE

--- src/dataset.py
+++ src/dataset.py
@@ -1,33 +1,9 @@
-'''
-Modality (01 = full-AV, 02 = video-only, 03 = audio-only).
-Vocal channel (01 = speech, 02 = song).
-Emotion (01 = neutral, 02 = calm, 03 = happy, 04 = sad, 05 = angry, 06 = fearful, 07 = disgust, 08 = surprised).
-Emotional intensity (01 = normal, 02 = strong). NOTE: There is no strong intensity for the 'neutral' emotion.
-Statement (01 = "Kids are talking by the door", 02 = "Dogs are sitting by the door").
-Repetition (01 = 1st repetition, 02 = 2nd repetition).
-Actor (01 to 24. Odd numbered actors are male, even numbered actors are female).
-
-'''
 import pandas as pd
-import numpy as np
-
 import os
-import sys
 
-# librosa is a Python library for analyzing audio and music. It can be used to extract the data from the audio files we will see it later.
-import librosa
-import librosa.display
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.model_selection import train_test_split
-
-# to play the audio files
-from IPython.display import Audio
 Tess = "data/TESS/tess toronto emotional speech set data/TESS Toronto emotional speech set data/"
-
+Savee = "data/SAVEE/ALL/"
+Ravdess = "data/RAVDESS/audio_speech_actors_01-24/"
 tess_directory_list = os.listdir(Tess)
 
 file_emotion = []
@@ -55,3 +31,40 @@ print(Tess_df.head())
 
 # Display the last few rows of Tess_df
 # print(Tess_df.tail())
+
+savee_directory_list = os.listdir(Savee)
+
+file_emotion = []
+file_path = []
+
+for file in savee_directory_list:
+    file_path.append(Savee + file)
+    part = file.split('_')[1]
+    ele = part[:-6]
+    if ele=='a':
+        file_emotion.append('angry')
+    elif ele=='d':
+        file_emotion.append('disgust')
+    elif ele=='f':
+        file_emotion.append('fear')
+    elif ele=='h':
+        file_emotion.append('happy')
+    elif ele=='n':
+        file_emotion.append('neutral')
+    elif ele=='sa':
+        file_emotion.append('sad')
+    else:
+        file_emotion.append('surprise')
+        
+# dataframe for emotion of files
+emotion_df = pd.DataFrame(file_emotion, columns=['Emotions'])
+
+# dataframe for path of files.
+path_df = pd.DataFrame(file_path, columns=['Path'])
+Savee_df = pd.concat([emotion_df, path_df], axis=1)
+print(Savee_df.head())
+
+
+data_path = pd.concat([Tess_df, Savee_df], axis = 0)
+data_path.to_csv("data_path.csv",index=False)
+data_path.head()
